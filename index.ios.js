0ada1319e62f9b9498ddc0ab0b2d1e42e24c3c7b
Ashwin Rao <theonlyrao@gmail.com>
2016-04-07 08:50:54 -0600

    before xcode makes changes

--- index.ios.js
+++ index.ios.js
@@ -3,50 +3,23 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-class PropertyFinder extends Component {
+var React = require('react-native')
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+})
+
+class PropertyFinderApp extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return React.createElement(React.Text, {style: styles.text}, "Hello, World!");
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
