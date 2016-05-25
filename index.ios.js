20b9d85c8a7f705aa76d84ea52fc5c236c7ee1f5
Ashwin Rao <theonlyrao@gmail.com>
2016-05-25 09:02:57 -0600

    Build search page
    Next up is Styling with Flexboxx

--- index.ios.js
+++ index.ios.js
@@ -1,25 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 'use strict';
 
-var React = require('react-native')
+var React = require('react-native');
+var SearchPage = require('./SearchPage');
 
-var styles = React.StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  }
-})
+var styles = React.StyleSheet.create({ 
+   text: {                              
+     color: 'black',                    
+     backgroundColor: 'white',          
+     fontSize: 30,                      
+     margin: 80                         
+   },
+    container: {
+	flex: 1
+    }
+ });                                    
 
 class PropertyFinderApp extends React.Component {
-  render() {
-    return React.createElement(React.Text, {style: styles.text}, "Hello, World!");
-  }
+    render () {
+	return (
+		<React.NavigatorIOS style={styles.container} initialRoute={{ title: 'Property Finder', component: SearchPage}}/>
+	);
+    }
 }
 
 React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
